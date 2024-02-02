f80db711efe543615d689cd3dbf4276123b25b34
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2024-02-02 17:53:55 +0600

    search

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -2,11 +2,44 @@ const Book = require("../models/book.model");
 
 const searchBook = async (req, res) => {
   try {
-    const topic = req.query.topic;
-    console.log(topic);
+    const { title, author, genre, price, id, sort, order } = req.query;
+
+    let query = {};
+    if (title) {
+      query.title = title;
+    }
+
+    if (genre) {
+      query.genre = genre;
+    }
+
+    if (id) {
+      query.id = id;
+    }
+    if (price) {
+      query;
+    }
+
+    if (author) {
+      query.author = author;
+    }
+
+    let sortOption = {};
+    if (sort) {
+      sortOption[sort] = order && order.toLowerCase() === "desc" ? -1 : 1;
+      sortOption.id = 1;
+    } else {
+      sortOption.id = order && order.toLowerCase() === "desc" ? -1 : 1;
+    }
+
+    const books = await Book.find(query)
+      .sort(sortOption)
+      .select({ _id: 0, __v: 0 });
+
+    res.status(200).json({ books });
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
