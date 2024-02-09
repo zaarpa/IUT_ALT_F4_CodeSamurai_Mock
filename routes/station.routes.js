110f61b1158d6604e6bedb398b243528f7464a81
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2024-02-09 17:04:25 +0600

    List all stations

--- routes/station.routes.js
+++ routes/station.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const stationController = require("../controllers/createStation.controller");
+const listStationsController = require("../controllers/allStations.controller");
 
 router.post("/api/stations", stationController.createStation);
+router.get("/api/stations", listStationsController.listStations);
 
 module.exports = router;
