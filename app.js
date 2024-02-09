c714f3f636c9c89f8c3f6cf0fa735e900f6d9cba
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2024-02-09 16:50:21 +0600

    modified app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const stationRoutes = require("./routes/station.routes");
 const userRoutes = require("./routes/user.routes");
-
+const trainRoutes = require("./routes/train.routes");
 app.use(express.json());
+app.use(stationRoutes);
 app.use(userRoutes);
-
+app.use(trainRoutes);
 mongoose
   .connect(process.env.ATLAS_URI)
   .then(() => {
