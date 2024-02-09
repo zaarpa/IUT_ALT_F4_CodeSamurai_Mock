78f5c407c5a6442a749eaa1b43a99f7908ebbdfc
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2024-02-09 17:12:20 +0600

    add wallet balance

--- routes/walletBalance.routes.js
+++ routes/walletBalance.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const walletController = require("../controllers/walletBalance.controller");
 
 router.get("/api/wallets/:wallet_id", walletController.getWalletBalance);
+router.put("/api/wallets/:wallet_id", walletController.addWalletBalance);
 
 module.exports = router;
